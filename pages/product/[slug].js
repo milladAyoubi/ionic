84abdea71e1f0de1fba2d5c347272628bf259d3f
Millad Ayoubi <Milladayoubi@gmail.com>
2023-03-01 19:02:25 -0500

    Product page update

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { client, urlFor } from '../../lib/client';
 import reviews from '../../styles/images/reviews.png';
-
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import { Product } from '../../components'; 
 
 const ProductDetails = ({product, products}) => {
     console.log(product)
@@ -19,12 +20,15 @@ const ProductDetails = ({product, products}) => {
                 <div className="product-details-text">
                     <h1>{name}</h1>
                     <div className="reviews-section">
-                    <span className="reviews">
-                    </span>
-                    
+                    <AiFillStar className="star-review"/>
+                    <AiFillStar className="star-review"/>
+                    <AiFillStar className="star-review"/>
+                    <AiFillStar className="star-review"/>
+                    <AiOutlineStar className="star-review"/>
+                    <p>(282)</p>
                     </div>
                     <p className="product-page-price">${price}</p>
-                    <p>{details}</p>
+                    <p className="product-details">{details}</p>
                     <br></br>
                     <p>Case Size: 46 mm</p>
                     <p>Case Water Resistance: 10 ATM</p>
@@ -32,17 +36,40 @@ const ProductDetails = ({product, products}) => {
 
 
                     <div className="color-group">
-                        <div className="color-group-item color-selector"><div className="color-caramel"></div></div>
-                        <div className="color-group-item "><div className="color-ivory "></div></div>
+                        <div className="color-group-item "><div className="color-caramel"></div></div>
+                        <div className="color-group-item color-selector"><div className="color-ivory "></div></div>
                         <div className="color-group-item"><div className="color-spastic"></div></div>
                         <div className="color-group-item "><div className="color-obsidian"></div></div>
                     </div>
-                    <button className="add-to-cart-button" >Add To Cart</button>
+                    <p className="color-name">Silver</p>
 
+                    <div className="quantity">
+                        <h4>Quantity</h4>
+                        <p className="quantity-desc">
+                            <span className="minus" onClick=""><AiOutlineMinus/></span>
+                            <span className="num" onClick="">1</span>
+                            <span className="plus" onClick=""><AiOutlinePlus/></span>
+
+                        </p>
+                    </div>
+
+                    <div className="buttons">
+                    <button type="button" className="buy-now" onClick="">Purchase Now</button>
+
+                    <button type="button" className="add-to-cart" onClick="">Add To Cart</button>
+                    </div>
                  </div> 
-                
-           
+        </div>
 
+        <div className="maylike-products-wrapper">
+            <h2>You May Also Like</h2>
+            <div className="marquee">
+                <div className="maylike-products-container">
+                    {products.map((item) => {
+
+                    })}
+                </div>
+            </div>
         </div>
     </div>
   )
