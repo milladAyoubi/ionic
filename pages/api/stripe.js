4f470964c1f9d6bb7ed0758c0d49e54341051ac3
Millad Ayoubi <Milladayoubi@gmail.com>
2023-08-09 23:05:05 -0400

    String Key

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
 
-const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const stripe = require('stripe')(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`);
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
